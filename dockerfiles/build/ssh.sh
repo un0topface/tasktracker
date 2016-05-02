c661ef4fcd2b08481be565c7c7295bc9d809fe6d
un0topface <y.myskin@topface.com>
2016-05-03 01:02:49 +0300

    Docker files upgrade

--- dockerfiles/build/ssh.sh
+++ dockerfiles/build/ssh.sh
@@ -9,5 +9,4 @@ echo "www-data ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 mkdir -m 700 -p /var/www/.ssh
 mkdir -m 700 -p /root/.ssh
 
-mv /build/config/ssh_host_rsa_key /etc/ssh/ssh_host_rsa_key
-chmod 644 /etc/ssh/ssh_host_rsa_key
+mv /build/config/ssh/* /etc/ssh/
