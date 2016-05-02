bd8b3632f0dd56d1f5e66a43a53532925f89aa0d
un0topface <y.myskin@topface.com>
2016-05-03 02:13:35 +0300

    Конфиги приложения

---
+++ lib/Config/Exception/ConfigNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Lib\Config\Exception;
+
+use Lib\Application\Exception\ApplicationException;
+
+/**
+ * Исключение, когда конфиг не найден
+ */
+class ConfigNotFoundException extends ApplicationException {
+
+}
