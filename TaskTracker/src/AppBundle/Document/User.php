dd86b808191a045a6982add3e936d6191bf6f2e6
un0topface <y.myskin@topface.com>
2016-05-17 12:22:12 +0300

    FOS USER BUNDLE

---
+++ TaskTracker/src/AppBundle/Document/User.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AppBundle\Document;
+
+use DateTime;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use MongoTimestamp;
+
+/**
+ * @MongoDB\Document
+ * @MongoDB\HasLifecycleCallbacks()
+ */
+class User extends BaseUser {
+    /**
+     * @MongoDB\Id(strategy="auto")
+     */
+    protected $id;
+
+    /**
+     * @MongoDB\String
+     * @var string
+     */
+    protected $fullName;
+
+    /**
+     * @MongoDB\String
+     * @var string
+     */
+    protected $created;
+
+    public function __construct() {
+        parent::__construct();
+        // your own logic
+    }
+
+    /**
+     * @MongoDB\prePersist
+     */
+    public function prePersist() {
+        $this->setCreated((new DateTime())->format('Y-m-d H:i:s'));
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreated() {
+        return $this->created;
+    }
+
+    /**
+     * @param string $created
+     * @return $this
+     */
+    public function setCreated($created) {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFullName() {
+        return $this->fullName;
+    }
+
+    /**
+     * @param string $fullName
+     * @return $this
+     */
+    public function setFullName($fullName) {
+        $this->fullName = $fullName;
+
+        return $this;
+    }
+}
